3416d3d1dcadcea0024e4a9138ba9ab7c002b23f
amordzon <a.mordzon.694@studms.ug.edu.pl>
2024-05-21 16:21:17 +0200

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { app } from "./app";
+import { app } from './app';
 
 const PORT = process.env.PORT || 430;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 export { server };
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,38 +1,38 @@
-import mongoose, { SchemaDefinition } from "mongoose";
-import validator from "validator";
+import mongoose, { SchemaDefinition } from 'mongoose';
+import validator from 'validator';
 
 const userSchemaDefinition: SchemaDefinition = {
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-    minlength: 3,
-    maxlength: 64,
-    trim: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    validate: {
-      validator: (value: string) => validator.isEmail(value),
-      message: "Please provide a valid email",
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        minlength: 3,
+        maxlength: 64,
+        trim: true,
+        unique: true,
     },
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    validate: {
-      validator: (value: string) => {
-        return /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{8,256})/.test(value);
-      },
-      message: "Wrong password format",
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        validate: {
+            validator: (value: string) => validator.isEmail(value),
+            message: 'Please provide a valid email',
+        },
+        unique: true,
     },
-  },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        validate: {
+            validator: (value: string) => {
+                return /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{8,256})/.test(value);
+            },
+            message: 'Wrong password format',
+        },
+    },
 };
 
 const userSchema = new mongoose.Schema(userSchemaDefinition, {
-  timestamps: true,
+    timestamps: true,
 });
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model('User', userSchema);
