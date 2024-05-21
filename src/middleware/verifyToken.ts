c16759bd2d1a7f45f1e3031c589ed0abef4fc1ef
justyna <towarnickaj@gmail.com>
2024-05-21 17:32:02 +0200

    fix: eslint

--- src/middleware/verifyToken.ts
+++ src/middleware/verifyToken.ts
@@ -1,67 +1,70 @@
-import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
-import { Request as ExpressRequest, Response, NextFunction } from "express";
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import { Request as ExpressRequest, Response, NextFunction } from 'express';
 
 dotenv.config();
 
 interface Request extends ExpressRequest {
-  username?: string;
+    username?: string;
 }
 
 export const verifyToken = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
+    req: Request,
+    res: Response,
+    next: NextFunction,
 ) => {
-  const authHeader = req.headers.authorization || req.headers.Authorization;
-  if (
-    !authHeader ||
-    (!Array.isArray(authHeader) && !authHeader.startsWith("Bearer "))
-  ) {
-    return res.status(403).send({ message: "No token provided" });
-  }
-
-  const token = Array.isArray(authHeader)
-    ? authHeader[0].split(" ")[1]
-    : authHeader.split(" ")[1];
-
-  const jwt_secret = process.env.JWT_SECRET;
-  if (!jwt_secret) {
-    throw new Error("JWT_SECRET is not defined in the environment variables");
-  }
-
-  const refresh_token_lifetime = process.env.REFRESH_TOKEN_LIFETIME;
-  if (!refresh_token_lifetime) {
-    throw new Error(
-      "REFRESH_TOKEN_LIFETIME is not defined in the environment variables",
-    );
-  }
-
-  jwt.verify(token, jwt_secret, (error: any, decoded: any) => {
-    if (error) {
-      return res.status(401).send({ message: "Unauthorized" });
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (
+        !authHeader ||
+        (!Array.isArray(authHeader) && !authHeader.startsWith('Bearer '))
+    ) {
+        return res.status(403).send({ message: 'No token provided' });
     }
-    req.username = decoded.username;
 
-    const rotateRefreshToken = jwt.sign(
-      { username: req.username },
-      jwt_secret,
-      {
-        expiresIn: refresh_token_lifetime,
-      },
-    );
+    const token = Array.isArray(authHeader)
+        ? authHeader[0].split(' ')[1]
+        : authHeader.split(' ')[1];
 
-    res.clearCookie("jwt", {
-      httpOnly: true,
-      // secure: true, // for https
-      sameSite: "none",
+    const jwt_secret = process.env.JWT_SECRET;
+    if (!jwt_secret) {
+        throw new Error(
+            'JWT_SECRET is not defined in the environment variables',
+        );
+    }
+
+    const refresh_token_lifetime = process.env.REFRESH_TOKEN_LIFETIME;
+    if (!refresh_token_lifetime) {
+        throw new Error(
+            'REFRESH_TOKEN_LIFETIME is not defined in the environment variables',
+        );
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    jwt.verify(token, jwt_secret, (error: any, decoded: any) => {
+        if (error) {
+            return res.status(401).send({ message: 'Unauthorized' });
+        }
+        req.username = decoded.username;
+
+        const rotateRefreshToken = jwt.sign(
+            { username: req.username },
+            jwt_secret,
+            {
+                expiresIn: refresh_token_lifetime,
+            },
+        );
+
+        res.clearCookie('jwt', {
+            httpOnly: true,
+            // secure: true, // for https
+            sameSite: 'none',
+        });
+        res.cookie('jwt', rotateRefreshToken, {
+            httpOnly: true,
+            // secure: true, // for https
+            sameSite: 'none',
+        });
+
+        next();
     });
-    res.cookie("jwt", rotateRefreshToken, {
-      httpOnly: true,
-      // secure: true, // for https
-      sameSite: "none",
-    });
-
-    next();
-  });
 };
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,23 +1,23 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
-  refreshToken,
-  register,
-  login,
-  logout,
-  changePassword,
-  validateRequest,
-} from "../controllers/auth.controller";
-import { verifyToken } from "../middleware/verifyToken";
+    refreshToken,
+    register,
+    login,
+    logout,
+    changePassword,
+    validateRequest,
+} from '../controllers/auth.controller';
+import { verifyToken } from '../middleware/verifyToken';
 
 const userRouter = Router();
 
-userRouter.post("/register", [verifyToken], register);
-userRouter.post("/login", login);
-userRouter.delete("/logout", logout);
+userRouter.post('/register', [verifyToken], register);
+userRouter.post('/login', login);
+userRouter.delete('/logout', logout);
 
-userRouter.patch("/change-password", [verifyToken], changePassword);
-userRouter.get("/refresh", refreshToken);
+userRouter.patch('/change-password', [verifyToken], changePassword);
+userRouter.get('/refresh', refreshToken);
 
-userRouter.get("/auth", [verifyToken], validateRequest);
+userRouter.get('/auth', [verifyToken], validateRequest);
 
 export default userRouter;
